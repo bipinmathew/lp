087a197fc8a8698be4adfe65ed453c3151942159
Bipin Mathew <bipinmathew@gmail.com>
2014-04-26 21:48:42 -0400

    Got basic simplex algorithm going

--- revised_simplex.m
+++ revised_simplex.m
@@ -9,11 +9,35 @@ function x = revised_simplex(A,b,c)
     [M,N] = size(A);
     % First we need to solve the augmented LP to get our first BFS
     % minimize 1'y
-    x = find_bfs(A,b)
+    [x,bv] = find_bfs(A,b)
+    dv = setdiff([1:N],bv)
+    r = c(dv).'-c(bv).'*inv(A(:,bv))*A(:,dv)
+    while ~isempty(find(r<0))
+        J = find(r<0)
+        JIDX = J(1)
+        J = dv(JIDX) % basis to enter 
+        ratios = b./A(:,J)
+        if isempty(find(ratios>=0))
+            error('Initial feasible solution could not be found. Phase I system is unbounded from below.');
+        else
+            min_ratios=min(ratios(find(ratios>=0)))
+            K=find(ratios==min_ratios)
+            KIDX = K(1)
+            K=bv(KIDX) % basis to leave
+            bv(KIDX) = J
+            dv(JIDX) = K
+            b = inv(A(:,bv))*b
+            r = c(dv).'-c(bv).'*inv(A(:,bv))*A(:,dv)
+        end
+
+    end
+
+    keyboard;
+
 end
 
 
-function x = find_bfs(A,b)
+function [x,bv] = find_bfs(A,b)
   b = b(:)
   [M,N] = size(A);
   B = eye(M);
@@ -23,9 +47,7 @@ function x = find_bfs(A,b)
   c = c(:)
   c(bv) = 1
   dv = setdiff([1:N+M],bv)
-  B = A(:,bv)
-  D = A(:,dv)
-  r = c(dv).'-c(bv).'*inv(B)*D
+  r = c(dv).'-c(bv).'*inv(A(:,bv))*A(:,dv)
   while ~isempty(find(r<0))
       J = find(r<0)
       JIDX = J(1)
@@ -40,10 +62,8 @@ function x = find_bfs(A,b)
           K=bv(KIDX) % basis to leave
           bv(KIDX) = J
           dv(JIDX) = K
-          B = A(:,bv)
-          D = A(:,dv)
-          b = inv(B)*b
-          r = c(dv).'-c(bv).'*inv(B)*D
+          b = inv(A(:,bv))*b
+          r = c(dv).'-c(bv).'*inv(A(:,bv))*A(:,dv)
       end
 
   end
